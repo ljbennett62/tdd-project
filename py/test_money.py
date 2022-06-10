683d5628c7e6d4f545e438b78298972c437e4357
ljbennett62 <lbenn@us.ibm.com>
2022-06-09 17:03:38 -0700

    refactor:moved Money and Portfolio classes to their own files

--- py/test_money.py
+++ py/test_money.py
@@ -1,47 +1,18 @@
 import unittest
-import functools
-import operator
 
-class Dollar:
-    def __init__(self, amount):
-        self.amount = amount
+from money import Money
+from portfolio import Portfolio
 
-    def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+# class Dollar:
+#    def __init__(self, amount):
+#        self.amount = amount
 
-class Money:
-    def __init__(self, amount, currency):
-        self.amount = amount
-        self.currency = currency
-
-    def times(self, multiplier):
-        return Money(self.amount * multiplier, self.currency)
-
-    def divide(self, divisor):
-        return Money(self.amount / divisor, self.currency)
-
-    def __eq__(self, other):
-        return self.amount == other.amount and self.currency == other.currency
-
-class Portfolio:
-    def __init__(self):
-        self.moneys = []
-    
-    def add(self, *moneys):
-        self.moneys.extend(moneys)
-
-    def evaluate(self, currency):
-        total = functools.reduce(
-                operator.add, map(lambda m: m.amount, self.moneys), 0)
-        return Money(total, currency)
+#    def times(self, multiplier):
+#        return Dollar(self.amount * multiplier)
 
 class TestMoney(unittest.TestCase):
-    def testMultiplicationInDollars(self):
-        fiveDollars = Money(5, "USD")
-        tenDollars = Money(10, "USD")
-        self.assertEqual(tenDollars, fiveDollars.times(2))
 
-    def testMultiplicationInEuros(self):
+    def testMultiplication(self):
         tenEuros = Money(10, "EUR")
         twentyEuros = Money(20, "EUR")
         self.assertEqual(twentyEuros, tenEuros.times(2))
